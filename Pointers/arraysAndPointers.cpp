36ad48aac57d9f0f99a2b36ad2cec82b4857a55e
iaayushmaan <116517217+iaayushmaan@users.noreply.github.com>
2023-07-13 17:29:46 +0530

    Create arraysAndPointers.cpp

---
+++ Pointers/arraysAndPointers.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // int arr[10];
+
+
+    // cout<<arr<<endl;
+    // cout<<&arr[0]<<endl;
+    // cout<<&arr[1]<<endl;
+    // cout<<arr[0]<<endl;
+    // cout<<*arr<<endl;
+    // cout<<*arr+1<<endl;
+    // cout<<(*arr+1)<<endl;
+    // cout<<endl;
+
+//CHARACTER ARRAY
+     
+    // char ch[6]="abcde";
+    // // char *c= &ch;
+    // cout<<ch<<endl;
+    // cout<<*ch<<endl;
+    // cout<<*ch + 1<<endl;
+    // cout<<*(ch+1)<<endl;
+
+    char temp='z';
+    char *p=&temp;
+    cout<<p<<endl;
+    cout<<*p<<endl;
+    cout<<&temp<<endl;
+    cout<<&p<<endl;
+
+}
